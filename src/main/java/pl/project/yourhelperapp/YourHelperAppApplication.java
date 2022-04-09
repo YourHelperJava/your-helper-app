820237229ab73fa4ceef6a196eda8ddb78249375
JakubN4 <90876767+JakubN4@users.noreply.github.com>
2022-04-09 10:30:34 +0200

    Create repository

---
+++ src/main/java/pl/project/yourhelperapp/YourHelperAppApplication.java
@@ -0,0 +1,13 @@
+package pl.project.yourhelperapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourHelperAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourHelperAppApplication.class, args);
+	}
+
+}
