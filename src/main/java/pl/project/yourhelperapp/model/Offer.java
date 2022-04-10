a4ee810db34f64283c0101358b6048b410d6c75a
Lukasz756 <lgasiorowski0000@gmail.com>
2022-04-10 15:03:57 +0200

    Added dependency data-jpa. Created model Offer.

---
+++ src/main/java/pl/project/yourhelperapp/model/Offer.java
@@ -0,0 +1,79 @@
+package pl.project.yourhelperapp.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Offer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String nameAndSurname;
+    private String localization;
+    private String date;
+    private String description;
+
+    public Offer() {
+    }
+
+    public Offer(Long id, String title, String nameAndSurname, String localization, String date, String description) {
+        this.id = id;
+        this.title = title;
+        this.nameAndSurname = nameAndSurname;
+        this.localization = localization;
+        this.date = date;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getNameAndSurname() {
+        return nameAndSurname;
+    }
+
+    public void setNameAndSurname(String nameAndSurname) {
+        this.nameAndSurname = nameAndSurname;
+    }
+
+    public String getLocalization() {
+        return localization;
+    }
+
+    public void setLocalization(String localization) {
+        this.localization = localization;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
