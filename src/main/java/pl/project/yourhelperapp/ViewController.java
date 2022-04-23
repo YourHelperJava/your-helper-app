1ae137317d25945342d15eb4fd076ca226c9d639
Jakub Nowak <90876767+JakubN4@users.noreply.github.com>
2022-04-23 14:00:19 +0200

    Update ViewController.java

--- src/main/java/pl/project/yourhelperapp/ViewController.java
+++ src/main/java/pl/project/yourhelperapp/ViewController.java
@@ -1,4 +1,4 @@
-package pl.project.yourhelperapp;
+package pl.project.yourhelperapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
