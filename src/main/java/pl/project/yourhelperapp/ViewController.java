188d14166cb4dae1d7c96ee9cf07b58610ac9eba
JakubN4 <90876767+JakubN4@users.noreply.github.com>
2022-04-23 13:47:56 +0200

    Add ViewController Change views directory

---
+++ src/main/java/pl/project/yourhelperapp/ViewController.java
@@ -0,0 +1,35 @@
+package pl.project.yourhelperapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+
+    @GetMapping("/")
+    public String getHome() {
+        return "offersList";
+    }
+
+    @GetMapping("/offersList")
+    public String getOffersList() {
+        return "offersList";
+    }
+
+    @GetMapping("/addNewOffer")
+    public String getAddNewOffer() {
+        return "addNewOffer";
+    }
+
+    @GetMapping("/detailedOffer")
+    public String getDetailedOffer() {
+        return "detailedOffer";
+    }
+
+    @GetMapping("/reservedOffersList")
+    public String getReservedOffersList() {
+        return "reservedOffersList";
+    }
+
+
+}
