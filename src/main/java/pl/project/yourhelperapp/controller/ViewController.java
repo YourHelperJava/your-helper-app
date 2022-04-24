010a287b349d1375e7096c5f86a056be10634351
JakubN4 <90876767+JakubN4@users.noreply.github.com>
2022-04-24 13:07:54 +0200

    Add AddNewOfferController Change @GetMapping for /AddNewOffer Add controller package

--- src/main/java/pl/project/yourhelperapp/controller/ViewController.java
+++ src/main/java/pl/project/yourhelperapp/controller/ViewController.java
@@ -1,7 +1,10 @@
-package pl.project.yourhelperapp;
+package pl.project.yourhelperapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import pl.project.yourhelperapp.model.Offer;
 
 @Controller
 public class ViewController {
@@ -17,7 +20,8 @@ public class ViewController {
     }
 
     @GetMapping("/addNewOffer")
-    public String getAddNewOffer() {
+    public String getAddNewOffer(Model model){
+        model.addAttribute("offer", new Offer());
         return "addNewOffer";
     }
 
